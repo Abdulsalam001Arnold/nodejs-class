16d99f60a988bacf1c696c3bbde3cd10d0342ec4
Abdulsalam001Arnold <abdulsalamabulhamidarnold@gmail.com>
2025-07-30 06:28:16 +0100

    Update cloudinary.js

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -3,9 +3,11 @@
 const cloudinary = require('cloudinary').v2
 
 cloudinary.config({
-    cloud_name:'dqpbdik90',
-    api_key:775479832557114,
-    api_secret:'rE282IwoaUR-UJ_-mS1qgur3s38'
+    cloud_name:process.env.CLOUD_NAME,
+    api_key:process.env.API_KEY,
+    api_secret:process.env.API_SECRET,
 })
+
+// console.log('The env secrets:', process.env.CLOUD_NAME, process.env.API_KEY, process.env.API_SECRET)
 
 module.exports = cloudinary
